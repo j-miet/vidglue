a7dd70622ce2608a7554b2cc8a10fb310d1e5dae
j-miet <191247923+j-miet@users.noreply.github.com>
2026-03-31 14:46:31 +0300

    Add gpu decoding baseline (currently too inefficient for use)

--- src/VideoInput.cpp
+++ src/VideoInput.cpp
@@ -1,18 +1,37 @@
+#include <chrono>
+#include <iostream>
 #include <stdexcept>
+
+extern "C" {
+#include <libavutil/hwcontext.h>
+}
 
 #include "Utils.hpp"
 #include "VideoInput.hpp"
+
+using std::runtime_error;
+
+AVPixelFormat VideoInput::get_hw_format(AVCodecContext* ctx,
+                                        const AVPixelFormat* pix_fmts) {
+    for (const enum AVPixelFormat* p = pix_fmts; *p != -1; p++) {
+        if (*p == AV_PIX_FMT_CUDA)
+            return *p;
+    }
+
+    std::cerr << "Warning: CUDA pixel format not offered, falling back\n";
+    return pix_fmts[0];
+}
 
 VideoInput::VideoInput(const std::string& filename) {
     AVFormatContext* fmt = nullptr;
 
     if (avformat_open_input(&fmt, filename.c_str(), nullptr, nullptr) < 0)
-        throw std::runtime_error("Failed to open input file");
+        throw runtime_error("Failed to open input file");
 
     m_format.reset(fmt);
 
     if (avformat_find_stream_info(m_format.get(), nullptr) < 0)
-        throw std::runtime_error("Failed to find stream info");
+        throw runtime_error("Failed to find stream info");
 
     const AVCodec* decoder = nullptr;
 
@@ -20,29 +39,37 @@ VideoInput::VideoInput(const std::string& filename) {
         m_format.get(), AVMEDIA_TYPE_VIDEO, -1, -1, &decoder, 0);
 
     if (m_streamIndex < 0)
-        throw std::runtime_error("No video stream found");
+        throw runtime_error("No video stream found");
 
     AVCodecContext* codec = avcodec_alloc_context3(decoder);
     if (!codec)
-        throw std::runtime_error("Failed to allocate codec");
+        throw runtime_error("Failed to allocate codec");
 
     m_decoder.reset(codec);
-    m_decoder->thread_count = 0; // multithreading, use all cores
-    m_decoder->thread_type = FF_THREAD_FRAME;
+    m_decoder->thread_count = 0;                                // multithreading, use all cores
+    m_decoder->thread_type = FF_THREAD_FRAME | FF_THREAD_SLICE; // let ffmpeg pick optimal model
+
+    // Keep this disabled for now, GPU decoding is currently inefficient
+    // checkDecoderHW();
 
     if (avcodec_parameters_to_context(
             m_decoder.get(),
             m_format->streams[m_streamIndex]->codecpar) < 0)
-        throw std::runtime_error("Failed to copy codec params");
+        throw runtime_error("Failed to copy codec params");
 
     if (avcodec_open2(m_decoder.get(), decoder, nullptr) < 0)
-        throw std::runtime_error("Failed to open decoder");
+        throw runtime_error("Failed to open decoder");
 
     m_frame.reset(av_frame_alloc());
     m_tempFrame.reset(av_frame_alloc());
 
     if (!m_frame || !m_tempFrame)
-        throw std::runtime_error("Failed to allocate frames");
+        throw runtime_error("Failed to allocate frames");
+}
+
+VideoInput::~VideoInput() {
+    if (m_hwDeviceContext)
+        av_buffer_unref(&m_hwDeviceContext);
 }
 
 bool VideoInput::decodeNextFrame() {
@@ -82,7 +109,17 @@ bool VideoInput::decodeNextFrame() {
         }
 
         av_frame_unref(m_frame.get());
-        av_frame_move_ref(m_frame.get(), m_tempFrame.get());
+
+        // GPU decocing is currently inefficient; don't call checkDecoderHW in constructor
+        // Reason is that av_hwframe_transfer_data need to
+        if (m_useHW && m_tempFrame->format == AV_PIX_FMT_CUDA) {
+            if (av_hwframe_transfer_data(m_frame.get(), m_tempFrame.get(), 0) < 0) {
+                throw runtime_error("Failed to transfer GPU frame to CPU");
+            }
+        } else {
+            av_frame_move_ref(m_frame.get(), m_tempFrame.get());
+        }
+
         m_decodedFrameIndex++;
         return true;
     }
@@ -90,7 +127,7 @@ bool VideoInput::decodeNextFrame() {
 
 double VideoInput::getDuration() const {
     if (!m_format || m_streamIndex < 0)
-        throw std::runtime_error("Invalid state");
+        throw runtime_error("Invalid state");
 
     AVStream* stream = m_format->streams[m_streamIndex];
     return stream->duration * av_q2d(stream->time_base);
@@ -102,4 +139,20 @@ int VideoInput::getVideoStreamIndex() const {
 
 int VideoInput::getAudioStreamIndex() const {
     return av_find_best_stream(m_format.get(), AVMEDIA_TYPE_AUDIO, -1, -1, nullptr, 0);
+}
+
+void VideoInput::checkDecoderHW() {
+    // Try to enable CUDA decoding
+    if (av_hwdevice_ctx_create(&m_hwDeviceContext,
+                               AV_HWDEVICE_TYPE_CUDA,
+                               nullptr, nullptr, 0) >= 0) {
+
+        m_decoder->hw_device_ctx = av_buffer_ref(m_hwDeviceContext);
+        m_decoder->get_format = get_hw_format; // set function pointer which ffmpeg calls internally
+        m_useHW = true;
+
+        std::cout << "Using GPU decoding (CUDA)\n";
+    } else {
+        std::cout << "GPU decoding not available, using CPU\n";
+    }
 }
--- src/VideoInput.hpp
+++ src/VideoInput.hpp
@@ -5,33 +5,47 @@ extern "C" {
 #include <libswscale/swscale.h>
 }
 
-#include "Structs.hpp"
-
 #include <memory>
+
+#include "Structs.hpp"
 
 class VideoInput {
   public:
     explicit VideoInput(const std::string& filename);
+    ~VideoInput(); // default constructor is used, but this overrides default move behavior i.e. removes it completely
 
-    bool decodeNextFrame();
+    // copy constructor must be overridden to prevent copying unique_ptr and causing errors
+    VideoInput(const VideoInput&) = delete;
+    VideoInput& operator=(const VideoInput&) = delete;
+
+    // move constructor must now be defined manually: copying is not allowed so default move behavior is desired
+    VideoInput(VideoInput&&) = default;
+    VideoInput& operator=(VideoInput&&) = default;
 
     AVFrame* getFrame() const { return m_frame.get(); }
+    AVFormatContext* getFormatContext() const { return m_format.get(); }
+    int getDecodedFrameIndex() const { return m_decodedFrameIndex; }
 
     double getDuration() const;
     int getVideoStreamIndex() const;
     int getAudioStreamIndex() const;
 
-    AVFormatContext* getFormatContext() const { return m_format.get(); }
-    int getDecodedFrameIndex() const { return m_decodedFrameIndex; }
+    bool decodeNextFrame();
+    void checkDecoderHW();
 
   private:
     std::unique_ptr<AVFormatContext, FormatDeleter> m_format;
     std::unique_ptr<AVCodecContext, CodecDeleter> m_decoder;
     std::unique_ptr<AVFrame, FrameDeleter> m_frame;
     std::unique_ptr<AVFrame, FrameDeleter> m_tempFrame;
-    std::unique_ptr<SwsContext, SwsDeleter> m_sws;
 
     int m_decodedFrameIndex{0};
-    int m_streamIndex{1};
+    int m_streamIndex{-1};
     bool m_eof{false};
+
+    AVBufferRef* m_hwDeviceContext{nullptr};
+    bool m_useHW{false};
+
+    static AVPixelFormat get_hw_format(AVCodecContext* ctx,
+                                       const AVPixelFormat* pix_fmts);
 };
